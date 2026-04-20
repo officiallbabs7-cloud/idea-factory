8abb7235d430193ddb78fc54a6b4bee338f01a96
Sergio Babs 77 <sergiobabs77@Sergios-MacBook-Air.local>
2026-04-20 08:21:47 +0100

    updates

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -18,8 +18,8 @@ const Statistics = () => {
         
 
         <div className="text-center flex-1 px-3 flex flex-col justify-between">
-          <h3 className="text-xl sm:text-xl font-semibold font-urbanist">Total Ideas Submitted</h3>
-          <h2 className="text-3xl sm:text-5xl mt-5 font-bold font-urbanist">684</h2>
+          <h3 className="text-xl sm:text-xl font-semibold font-urbanist">Total Approved Ideas</h3>
+          <h2 className="text-3xl sm:text-5xl mt-5 font-bold font-urbanist">37</h2>
         </div>
 </div>
       </div>
